937b2bebf8ebf40a188a5f3af9a9a70c1b6520de
alexwinx <winxalex@gmail.com>
2019-11-25 01:48:28 +0100

    add timer add tests

--- Main.py
+++ Main.py
@@ -6,18 +6,18 @@ from py_expression_eval import Parser
 import pyowm
 from scrapers.scraper import get_driver, connect_to_site, \
     parse_html
+import threading
 
-
-from storages.storage import create_connection,create_table,\
-    data_entry,data_print,data_delete
+from storages.storage import create_connection, create_table, \
+    data_entry, data_print, data_delete
 
 #todo load from config
 
-page_url="https://www.viennaairport.com/passagiere/ankunft__abflug/abfluege"
-driver_url="D:\Projects\PythonScraper\chromedriver.exe"
-db_url= r"D:\Projects\PythonScraper\scrapped.db"
-weather_app_id= '4832c5bf5984665cd39ee2d7308311ac'
-sql_table_name="scrapped"
+page_url = "https://www.viennaairport.com/passagiere/ankunft__abflug/abfluege"
+driver_url = "D:\Projects\PythonScraper\chromedriver.exe"
+database_url = r"D:\Projects\PythonScraper\scrapped.db"
+weather_app_id = '4832c5bf5984665cd39ee2d7308311ac'
+sql_table_name = "scrapped"
 
 #destination, time, temperature and note into a database
 sql_create_scrapped_table = """ CREATE TABLE IF NOT EXISTS scrapped (
@@ -28,26 +28,19 @@ sql_create_scrapped_table = """ CREATE TABLE IF NOT EXISTS scrapped (
                                         note text
                                     ); """
 
-sql_insert_scrapped_table="INSERT INTO scrapped (destination, time, temperature, note) VALUES (?,?,?,?)"
+sql_insert_scrapped_table = "INSERT INTO scrapped (destination, time, temperature, note) VALUES (?,?,?,?)"
 
 #from .csv or .db
 #if >25 C in London, note = "Let's go for a pint" ○ if <5 C in Madrid, note = "!Que frio!"
-note_rules=list()
-note_rules.append({"rule":"t>25 and city=='London'","note":"Let's go for a pint"})
-note_rules.append({"rule":"t<10 and city=='London'","note":"Frizzing"})
-note_rules.append({"rule":"t<5 and city=='Madrid'","note":"!Que frio!"})
-note_rules.append({"rule":"t<5 and city=='Moskau'","note":"!FFFFFF!"})
+note_rules = list()
+note_rules.append({"rule": "t>25 and city=='London'", "note": "Let's go for a pint"})
+note_rules.append({"rule": "t<10 and city=='London'", "note": "Frizzing"})
+note_rules.append({"rule": "t<5 and city=='Madrid'", "note": "!Que frio!"})
+note_rules.append({"rule": "t<5 and city=='Moskau'", "note": "!FFFFFF!"})
+
 
 #print(parser.parse(note_rules[0]['rule']).evaluate({"t":3,"city":'London'}))
 
-#every hour
-#from crontab import CronTab
-#
-#cron = CronTab(user='username')
-#job = cron.new(command='E:\\Program Files\\Python3.8\\python D:\\Projects\\PythonScraper\\everyHour.py')
-#job.hour.every(1)
-#
-#cron.write()
 
 
 
@@ -65,7 +58,8 @@ note_rules.append({"rule":"t<5 and city=='Moskau'","note":"!FFFFFF!"})
 
 
 
-def get_temp(o,loc):
+
+def get_temp(o, loc):
     try:
         # Search for current weather in
         observation = o.weather_at_place(loc)
@@ -75,64 +69,59 @@ def get_temp(o,loc):
         return w.get_temperature('celsius')['temp']
 
     except Exception as ex:
-        print("Retrieving temperature failed for location "+loc)
+        print("Retrieving temperature failed for location " + loc)
     return -255
 
 
-
-
 def evaluate_rules(city, temp, n_rules):
-
     parser = Parser()
 
     for note_rule in n_rules:
-        if parser.parse(note_rule['rule']).evaluate({"t":temp,"city":city}):
+        if parser.parse(note_rule['rule']).evaluate({"t": temp, "city": city}):
             return note_rule["note"]
 
     return ''
 
-def run_process(p_url,d_url, db_url):
+
+def run_process(p_url, d_url, db_url):
 
     browser = get_driver(d_url)
-    
+
     if connect_to_site(browser, p_url):
 
         html = browser.page_source
 
         #print(html)
-        locations=parse_html(html)
+        locations = parse_html(html)
 
         #print(locations)
 
         owm = pyowm.OWM(weather_app_id)  # You MUST provide a valid API key
 
-
         #create a database connection
         conn = create_connection(db_url)
 
-
         if conn is not None:
 
-            data_delete(conn,sql_table_name)
+            data_delete(conn, sql_table_name)
 
             #
-            create_table(conn,sql_create_scrapped_table)
+            create_table(conn, sql_create_scrapped_table)
             #data_entry(conn,sql_insert_scrapped_table,[city,datetime.datetime.now().strftime('%Y%m%d%H%M%S'),12,"test note"])
 
             for location in locations:
-                temp=get_temp(owm,location)
-                if temp >-255:
-                    city=location.split(',')[0]
-                    data_entry(conn,sql_insert_scrapped_table,[city,datetime.datetime.now().strftime('%Y%m%d%H%M%S'),temp,evaluate_rules(city,temp,note_rules)])
+                temp = get_temp(owm, location)
+                if temp > -255:
+                    city = location.split(',')[0]
+                    data_entry(conn, sql_insert_scrapped_table,
+                               [city, datetime.datetime.now().strftime('%Y%m%d%H%M%S'), temp,
+                                evaluate_rules(city, temp, note_rules)])
 
+            data_print(conn, sql_table_name)
 
-            data_print(conn,sql_table_name)
+            conn.close()
         else:
             print("Error! cannot create the database connection.")
-
-
-
-
 
         #print(html)
         browser.quit()
@@ -141,18 +130,29 @@ def run_process(p_url,d_url, db_url):
         browser.quit()
 
 
+def scrap_process():
+    start_time = time()
 
+    run_process(page_url, driver_url, database_url)
 
+    end_time = time()
+    elapsed_time = end_time - start_time
+    print(f'Elapsed run time: {elapsed_time} seconds')
 
-requests_cache.install_cache(expire_after=3600) #after 1hour
+def scrap_every(sec):
+    threading.Timer(sec, scrap_every, args=[sec]).start()
+    scrap_process()
 
+requests_cache.install_cache(expire_after=3600)  #after 1hour
 
+#every hour
+#from crontab import CronTab
+#
+#cron = CronTab(user='username')
+#job = cron.new(command='E:\\Program Files\\Python3.8\\python D:\\Projects\\PythonScraper\\everyHour.py')
+#job.hour.every(1)
+#
+#cron.write()
+#scrap_every(3600)
+scrap_every(5)
 
-start_time = time()
-
-run_process(page_url,driver_url,db_url)
-
-
-end_time = time()
-elapsed_time = end_time - start_time
-print(f'Elapsed run time: {elapsed_time} seconds')
--- test/test_scraper.py
+++ test/test_scraper.py
@@ -1,14 +1,12 @@
 import unittest
 from unittest.mock import patch
-from scrapers.scraper import parse_html
+from scrapers.scraper import parse_html,get_driver
+from storages.storage import create_connection
 
 
 class TestParseFunction(unittest.TestCase):
 
-    # def setUp(self):
-    #     with open('test/test.html', encoding='utf-8') as f:
-    #         html = f.read()
-    #         self.output = parse_html(html)
+
 
     @patch('scrapers.scraper.get_load_time')
     def setUp(self, mock_get_load_time):
@@ -16,6 +14,9 @@ class TestParseFunction(unittest.TestCase):
         with open('test.html', encoding='utf-8') as f:
             html = f.read()
             self.output = parse_html(html)
+        self.driver_url="D:\Projects\PythonScraper\chromedriver.exe"
+        self.db_url= r"D:\Projects\PythonScraper\scrapped.db"
+
 
     def tearDown(self):
         self.output = []
@@ -26,8 +27,17 @@ class TestParseFunction(unittest.TestCase):
     def test_output_is_a_list(self):
         self.assertTrue(isinstance(self.output, list))
 
-    def test_output_is_a_list_of_dicts(self):
-        self.assertTrue(all(isinstance(elem, dict) for elem in self.output))
+    def test_output_is_a_list_of_string(self):
+        self.assertTrue(all(isinstance(elem, str) for elem in self.output))
+
+    def test_brower(self):
+        browser=get_driver(self.driver_url)
+        self.assertTrue(browser is not None)
+
+    def test_db(self):
+        conn=create_connection(self.db_url)
+        self.assertTrue(conn is not None)
+        conn.close()
 
 
 if __name__ == '__main__':
